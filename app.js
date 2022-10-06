935be87c27798a403941cce33db9b6c2a39dd3a9
nickbloo <nickbloo2@gmail.com>
2022-10-05 20:46:32 -0500

    submit and input added

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 console.log("Test message");
+
+const gameState = {
+    marks: ["x", "o"],
+    gameBoard: [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+    ],
+    playerScores: [0, 0],
+    playerName: ""
+};
+
+let gameBoardElement = document.getElementById("game-board");
+let displayName = document.getElementById("display-name");
+let nameInput = document.getElementById("name-input");
+let submitButton = document.getElementById("submit-button");
+
+function displayNameFunction() {
+    gameState.playerName = nameInput.value; 
+
+    displayName.textContent = `Your Name: ${gameState.playerName}`;
+
+    nameInput.value = ""
+}
+
+submitButton.addEventListener("click", displayNameFunction);
